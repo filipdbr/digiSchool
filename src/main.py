2e6fe0f31cff392d6b9c2ad32dbca7bf21d17612
Filip <filip.a.dabrowski@gmail.com>
2024-09-27 14:42:57 +0200

    Changed the structure

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -11,3 +9,4 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
