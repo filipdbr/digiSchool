d092979bb0b68948d44cbd74e73cfef53f68cbdf
Filip <filip.a.dabrowski@gmail.com>
2024-10-01 16:06:51 +0200

    updated the professor schema

--- src/schemas/professor_schema.py
+++ src/schemas/professor_schema.py
@@ -2,36 +2,12 @@ from pydantic import BaseModel
 from typing import List, Optional
 from datetime import datetime
 
-
-class ProfessorSchema(BaseModel):
-    """
-    Represents a professor entity for data validation.
-
-    Attributes:
-        prof_id (Optional[int]): Unique identifier for the professor.
-        last_name (Optional[str]): Last name of the professor.
-        first_name (Optional[str]): First name of the professor.
-        birth_date (Optional[datetime]): Birth date of the professor.
-        address (Optional[str]): Address of the professor.
-        gender (Optional[str]): Gender of the professor.
-
-    Config:
-        orm_mode (bool): Enables compatibility with ORM objects (e.g., SQLAlchemy models).
-    """
-    teacher_id: Optional[int] = None
-    last_name: Optional[str] = None
-    first_name: Optional[str] = None
-    date_of_birth: Optional[datetime] = None
-    address: Optional[str] = None
-    gender: Optional[str] = None
-
-    class Config:
-        from_attributes = True # Enables using instances of SQLAlchemy models directly
-
+from src.models import Professor
 
 class ProfessorResponse(BaseModel):
     """
     Represents a professor entity for the API response. We decided to exclude some data.
+    Response is defined as first as it possesses fewer attributes than ProfessorSchema, hence ProfessorSchema.
 
     Attributes:
         last_name (str): Last name of the professor.
@@ -43,6 +19,19 @@ class ProfessorResponse(BaseModel):
     first_name: str
     address: str
     gender: str
+    date_of_birth: Optional[datetime] = None
 
     class Config:
         from_attributes = True  # Enables using instances of SQLAlchemy models directly
+
+class ProfessorSchema(ProfessorResponse):
+    """
+    Represents a professor entity for data validation.
+
+    Attributes:
+        teacher_id (Optional[int]): Unique identifier for the professor used for data validation.
+
+    Config:
+        orm_mode (bool): Enables compatibility with ORM objects (e.g., SQLAlchemy models).
+    """
+    teacher_id: Optional[int] = None
