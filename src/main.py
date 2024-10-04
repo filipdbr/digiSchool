55f68ffa507bd6dd6b72f8331bf705128dcdc534
Filip <filip.a.dabrowski@gmail.com>
2024-10-04 15:07:28 +0200

    Added a main page

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-# main.py
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from src.routers.student_router import router as student_router
 from src.routers.professor_router import router as professor_router
@@ -8,8 +7,25 @@ from src.routers.class_router import router as class_router
 
 app = FastAPI()
 
-# Register routers
-app.include_router(professor_router)
-app.include_router(student_router)
-app.include_router(class_router)
+# main page
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome to DigiSchool</title>
+    </head>
+    <body>
+        <h1>Welcome to DigiSchool FastAPI App</h1>
+        <p>To explore and test the API endpoints, visit <a href="/docs">/docs</a>.</p>
+    </body>
+    </html>
+    """
 
+# routers
+app.include_router(student_router)
+app.include_router(professor_router)
+app.include_router(class_router)
